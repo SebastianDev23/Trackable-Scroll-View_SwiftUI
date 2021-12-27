94ce3824af47a176bdbaa086e8ba6980d057a29d
Sebastian <s.kloesel@gmx.de>
2021-12-27 16:49:47 +0100

    Initial Commit

---
+++ TrackableScrollView_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackableScrollView_SwiftUI
+//
+//  Created by Sebastian Klösel on 27.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrackableScrollView_SwiftUI/TrackableScrollView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackableScrollView_SwiftUIApp.swift
+//  TrackableScrollView_SwiftUI
+//
+//  Created by Sebastian Klösel on 27.12.21.
+//
+
+import SwiftUI
+
+@main
+struct TrackableScrollView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
